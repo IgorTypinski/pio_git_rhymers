dd2c5570f6e3c0d2fbc1f66961a95ac3f9bf0039
Wojciech Kłos <mail>
2025-04-02 20:13:52 +0200

    Zastosowanie się do zmian zasugerowanych w Issues

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,35 +2,36 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int INT = 12;
-    public static final int INT1 = -1;
-    public static final int INT2 = 11;
-    private int[] numbers = new int[INT];
+    public static final int CAPACITY = 12;
+    public static final int MAX = 11;
+    public static final int EMPTY = -1;
+    public static final int RETURN = -1;
+    private final int[] numbers = new int[CAPACITY];
 
-    public int total = INT1;
+    public int total = EMPTY;
 
-    public void countIn(final int in) {
+    public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
-    public final boolean callCheck() {
-        return total == INT1;
+    public boolean callCheck() {
+        return total == EMPTY;
     }
 
-    public final boolean isFull() {
-        return total == INT2;
+    public boolean isFull() {
+        return total == MAX;
     }
 
-    protected final int peekaboo() {
+    protected int peekaboo() {
         if (callCheck())
-            return INT1;
+            return RETURN;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return INT1;
+            return RETURN;
         return numbers[total--];
     }
 
