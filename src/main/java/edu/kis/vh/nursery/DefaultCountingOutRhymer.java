44ed7fd4c0910fbfa883597cc322f2674dd4d2c4
unknown <251237@edu.p.lodz.pl>
2025-04-09 19:09:40 +0200

    2.12 usuniecie nieuzywanego settera

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -55,8 +55,4 @@ public class DefaultCountingOutRhymer {
     public int[] getNumbers() {
         return numbers;
     }
-
-    public void setTotal(int total) {
-        this.total = total;
-    }
 }
